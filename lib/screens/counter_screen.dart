de36a57e97b3d206268b70b16bad5aa40a217762
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-05-23 00:24:17 -0300

    added button to increase and decrease

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -1,60 +1,67 @@
 import 'package:flutter/material.dart';
 
 class CounterScreen extends StatefulWidget {
-  const CounterScreen ({Key? key}) : super(key: key);
-
-  
+  const CounterScreen({Key? key}) : super(key: key);
 
   @override
   State<CounterScreen> createState() => _CounterScreenState();
 }
 
 class _CounterScreenState extends State<CounterScreen> {
-
   int counter = 0;
   @override
   Widget build(BuildContext context) {
-
     const fontSize30 = TextStyle(fontSize: 30);
-    
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('CounterScreen'),
-          elevation: 10,
-          centerTitle: true,
+      appBar: AppBar(
+        title: const Text('CounterScreen'),
+        elevation: 10,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Numero de clicks', style: fontSize30),
+            Text('$counter', style: fontSize30)
+          ],
         ),
-        body:Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              
-              children: [
-                const Text('Numero de clicks', style: fontSize30), 
-                Text('$counter', style: fontSize30)
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
 
-                
-                ],
-            ),
-        ),
+          FloatingActionButton(
+            child: const Icon(Icons.exposure_minus_1),
+            onPressed: () => setState(() => counter--),
+          ),
 
-        // floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: FloatingActionButton(
+
+          //const SizedBox(
+           // width: 20,
+          //),
+
+
+          FloatingActionButton(
+            child: const Icon(Icons.restart_alt_outlined),
+            onPressed: () => setState(() => counter=0),
+            
+          ),
+
+
+         // const SizedBox(
+          //  width: 20,
+          //),
+
           
-          child: const Icon(Icons.add),
-          
-          onPressed: () { 
-
-            // print('HolaMundo');
-            counter++;
-
-            setState(() {
-              
-            });
-
-           },
-
-
-        ),
-        );
+          FloatingActionButton(
+            child: const Icon(Icons.exposure_plus_1_outlined),
+            onPressed: () => setState(() => counter++),
+          ),
+        ],
+      ),
+    );
   }
 }
